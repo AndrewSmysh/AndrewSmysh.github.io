2f226c308ed1a9f0774cb9ee7da5c537b31f9651
AndrewSmysh <65419283+AndrewSmysh@users.noreply.github.com>
2023-09-21 17:32:46 +0300

    Add files via upload

--- scripts/login-register.js
+++ scripts/login-register.js
@@ -132,4 +132,13 @@ window.addEventListener("load", function () {
     }
 });
 
+window.addEventListener("load", function () {
+    var isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (isLoggedIn) {
+        document.querySelector(".asside-login").style.display = "none";
+        document.querySelector(".asside-user").style.display = "flex";
+    } else {
+        document.querySelector(".asside-user").style.display = "none";
+    }
+});
 
